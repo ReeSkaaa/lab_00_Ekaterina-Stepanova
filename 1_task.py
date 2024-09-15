5f5a8accb68b2e72b2d96c561ae7a2360720e09f
ReeSkaaa <160928530+ReeSkaaa@users.noreply.github.com>
2024-09-15 12:37:07 +0300

    kklkk

---
+++ 1_task.py
@@ -0,0 +1,17 @@
+import time
+from memory_profiler import profile
+@profile
+
+
+
+def main():
+    a, b = map(int, input().split())
+    if -10 ** 9 <= a <= 10 ** 9 and -10 ** 9 <= b <= 10 ** 9:
+        print(a + b)
+
+if __name__ == '__main__':
+    main()
+t_start = time.perf_counter()
+print("Время работы: %s секунд" % (time.perf_counter() - t_start))
+
+
---
+++ 4_task.py
@@ -0,0 +1,5 @@
+import time
+import os
+t_start = time.perf_counter()
+
+f = open(os.path.join(os.path.dirname(_file_), "input.txt"), 'r')
