f77e8bcc3000020a81b9937be4d23eed20e47670
ReeSkaaa <160928530+ReeSkaaa@users.noreply.github.com>
2024-09-22 22:56:27 +0300

    Update 1_task.py

--- 1_task.py
+++ 1_task.py
@@ -8,10 +8,13 @@ def main():
     a, b = map(int, input().split())
     if -10 ** 9 <= a <= 10 ** 9 and -10 ** 9 <= b <= 10 ** 9:
         print(a + b)
+    quit('Введите еще раз')
 
 if __name__ == '__main__':
     main()
 t_start = time.perf_counter()
 print("Время работы: %s секунд" % (time.perf_counter() - t_start))
+
+
 
 
