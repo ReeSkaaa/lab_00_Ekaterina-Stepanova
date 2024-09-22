61d62da56b9f2d144fb7d8e561cdd51569fc56a6
ReeSkaaa <160928530+ReeSkaaa@users.noreply.github.com>
2024-09-22 22:56:53 +0300

    Update 2_task.py

--- 2_task.py
+++ 2_task.py
@@ -7,15 +7,12 @@ from memory_profiler import profile
 
 @profile
 def main():
-
-
     def fib(n):
         f_prev = 0
         f_new = 1
         for k in range(n):
             f_prev, f_new = f_new, f_new + f_prev
         return f_prev
-
 
     f1 = open('input.txt', 'r')
     n = int(f1.readline())
@@ -24,7 +21,12 @@ def main():
         f2 = open('output.txt', 'w')
         f2.write(str(fib(n)))
         f2.close()
+    else:
+        quit('Введите еще раз')
+
+
 if __name__ == '__main__':
     main()
 t_start = time.perf_counter()
 print("Время выполнения: %s секунд" % (time.perf_counter() - t_start))
+
