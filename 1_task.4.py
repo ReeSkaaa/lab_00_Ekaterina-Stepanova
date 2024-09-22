9e14f0834956073040bb9b4f19b3fc81c583f5c8
ReeSkaaa <160928530+ReeSkaaa@users.noreply.github.com>
2024-09-22 22:56:04 +0300

    Update 1_task.4.py

--- 1_task.4.py
+++ 1_task.4.py
@@ -8,6 +8,8 @@ def main():
     a, b = int(a), int(b)
     if -10 ** 9 <= a <= 10 ** 9 and -10 ** 9 <= b <= 10 ** 9:
         open('output.txt', 'w').write(str(a + b ** 2))
+    else:
+        quit('Введите еще раз')
 if __name__ == '__main__':
     main()
 t_start = time.perf_counter()
