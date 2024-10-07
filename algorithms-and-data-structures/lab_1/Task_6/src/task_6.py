a16630da2f77b88d0c0ae1996224f40698c44302
Your Name <katyast0505@gmail.com>
2024-10-07 19:17:10 +0300

    add lab_1

--- algorithms-and-data-structures/lab_1/Task_6/src/task_6.py
+++ algorithms-and-data-structures/lab_1/Task_6/src/task_6.py
@@ -6,10 +6,10 @@ t_start = time.perf_counter()
 
 @profile()
 def bubble_sort(a):
-    for i in range(1, len(a)):
-        for j in range(len(a) - 1, i + 1, -1):
-            if a[j] < a[j - 1]:
-                a[j], a[j - 1] = a[j - 1], a[j]
+    for i in range(len(a) - 1):
+        for j in range(len(a) - i - 1):
+            if a[j + 1] < a[j]:
+                a[j], a[j + 1] = a[j + 1], a[j]
     return a
 
 
