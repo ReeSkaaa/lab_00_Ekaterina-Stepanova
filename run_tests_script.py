6035ee62583715d4a2d2a9ab5e90b3574d662a6b
Your Name <katyast0505@gmail.com>
2024-11-20 14:28:14 +0300

    add lab_3

--- run_tests_script.py
+++ run_tests_script.py
@@ -1,27 +1,8 @@
 import subprocess
+from colorama import Fore, Back, Style
 import os
 
-
-# запуск тестов первой лабы
-tasks = ['1', '3', '4', '5', '6', '8', '9']
-for i in tasks:
-    print(' ')
-    print('######## Lab_1', f'Task_{i}', '###########################################################')
-    PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'lab_1', f'Task_{i}', 'tests', 'test.py'))
-    subprocess.run("python " + PATH, shell=True)
-
-# запуск тестов второй лабы
-tasks = ['1', '3', '4', '5', '6', '7']
-for i in tasks:
-    print(' ')
-    print('######## Lab_2', f'Task_{i}', '###########################################################')
-    PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'lab_2', f'Task_{i}', 'tests', 'test.py'))
-    subprocess.run("python " + PATH, shell=True)
-
-#запуск третьей лабы
-tasks = ['1', '2', '3', '5', '6', '7']
-for i in tasks:
-    print(' ')
-    print('######## Lab_3', f'Task_{i}', '###########################################################')
-    PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'lab_3', f'Task_{i}', 'tests', 'test.py'))
+for i in range(1, 4):
+    print(Fore.GREEN +'-------Запуск тестов для', f'lab_{i}', '------------------------------------------' + Style.RESET_ALL)
+    PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), f'lab_{i}', 'run_tests_script.py'))
     subprocess.run("python " + PATH, shell=True)
